c85bef7e11ce15b7122d5e6ecd1dcc1ad8d752fc
Enea Dhack <enea@vaened.dev>
2025-03-17 12:38:48 -0500

    feat: improve constructor in Expressions class
    Refactored the constructor in the Expressions class to separate filters from expressions and handle them differently for better organization and readability. Added filtering logic to handle filters separately and merge them back into the list if needed. Emphasized the importance of clear separation between filters and expressions for better code maintenance. 🚀

--- src/Expressions.php
+++ src/Expressions.php
@@ -8,22 +8,40 @@ declare(strict_types=1);
 namespace Vaened\CriteriaCore;
 
 use Vaened\CriteriaCore\Directives\Expression;
+use Vaened\CriteriaCore\Directives\Filter;
 use Vaened\Support\Types\SecureList;
+
+use function array_merge;
+use function count;
+use function Lambdish\Phunctional\filter;
 
 final class Expressions extends SecureList
 {
+    public function __construct(iterable $expressions)
+    {
+        $filters     = filter(static fn(mixed $item) => $item instanceof Filter, $expressions);
+        $expressions = filter(static fn(mixed $item) => !$item instanceof Filter, $expressions);
+
+        parent::__construct(
+            array_merge(
+                $expressions,
+                count($filters) > 1 ? Statements::of($filters) : []
+            )
+        );
+    }
+
     public static function from(iterable $scopes): self
     {
         return new self($scopes);
     }
 
-    public function push(Expression $expression): void
-    {
-        $this->items[] = $expression;
-    }
-
     public static function type(): string
     {
         return Expression::class;
+    }
+
+    public function push(Expression $expression): void
+    {
+        $this->items[] = $expression;
     }
 }
